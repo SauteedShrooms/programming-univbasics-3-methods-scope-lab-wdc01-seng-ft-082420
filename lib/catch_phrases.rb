f3c2974d7847d3a1589ecec3d1686a52bf78b5a9
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-06 20:48:03 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,11 +3,13 @@ require 'pry'
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
+  phrase
 end
 binding.pry
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+  status
 end
 binding.pry
